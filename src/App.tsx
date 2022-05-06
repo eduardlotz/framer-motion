57ee732171cd4ff010bbd5a46a56bfcb2a177233
Eduard Lotz <lobros@live.de>
2022-05-06 09:08:08 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useLocation } from "react-router-dom";
+import { HomePage, ExamplesPage, ErrorPage } from "./pages";
+import styled from "styled-components";
+import { Navigation } from "./components/molecules";
+import { AboutPage } from "./pages/about";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      <Container>
+        <AnimatePresence exitBeforeEnter>
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/examples" element={<ExamplesPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </AnimatePresence>
+      </Container>
+    </>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  max-height: 100%;
+  height: calc(100vh - 112px);
+`;
